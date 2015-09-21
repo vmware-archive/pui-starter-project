8e6e321198fdca37e912948d11865979d80d8024
August Toman-Yih <atomanyih@pivotal.io>
2015-09-21 10:31:53 -0700

    spike: card grid layout
    Signed-off-by: Caroline Taymor <ctaymor@pivotal.io>

--- myComponents.js
+++ myComponents.js
@@ -6,17 +6,72 @@ var React = require('react');
 // Required modules here like so:
 // var DefaultButton = require('pui-react-buttons').DefaultButton;
 
+var Card = React.createClass({
+  render() {
+    return (
+      <div className="card" style={this.props.style}>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+var Grid = React.createClass({
+  render() {
+    let rows = [];
+    //for (let i = 0; i < this.props.children.length; i += this.props.itemsPerRow) {
+    //  rows.push(this.props.children.slice(i, i + this.props.itemsPerRow));
+    //}
+
+    rows.push(this.props.children);
+
+    const wrappedRows = rows.map((row) => {
+      return (
+        <div className="gridRow">{row}</div>
+      );
+    });
+
+    return (
+      <div className={`grid grid-${this.props.itemsPerRow}`}>
+        {wrappedRows}
+      </div>
+    );
+  }
+});
+
+Grid.Item = React.createClass({
+  render() {
+    return (
+      <div className="item">
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
 var MyTestPage = React.createClass({
   render: function() {
     return (
-      // Put your react components here, like so:
-      // <DefaultButton>
-      //   I'm a button to click!
-      // </DefaultButton>
-      <p> Your react components will render here! </p>
+      <Grid itemsPerRow={4}>
+        <Grid.Item>
+          <Card style={{height: '150px'}}>I am a card that is really really really really big</Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Card>I yam a card</Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Card>I ham a card</Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Card>I am</Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Card>I am not</Card>
+        </Grid.Item>
+      </Grid>
     )
   }
+
 });
 
 React.render(<MyTestPage />, document.getElementById('root'));
