db8c3a750c4d1619ac26c275cb0156471e3439f5
Caroline Taymor <ctaymor@pivotal.io>
2015-09-22 07:58:38 -0700

    WIP: Tilelayout component
    [#103851000]
    
    Signed-off-by: August Toman-Yih <atomanyih@pivotal.io>

---
+++ js/tileLayout.js
@@ -0,0 +1,7 @@
+var React = require('react');
+
+export const TileLayout = React.createClass({
+  render() {
+
+  }
+});
--- spec/demo_spec.js
+++ spec/demo_spec.js
@@ -1,3 +1,12 @@
-it('should have truth', function() {
-  expect(true).toBeTruthy();
+var TileLayout = require('../js/tileLayout').TileLayout;
+
+describe('TileLayout', () => {
+  it('creates tile-layout')
+  it('creates tile-items')
+  describe('columns', () => {
+    it('adds classes')
+  });
+  describe('gutters', () => {
+    it('adds class')
+  })
 });
